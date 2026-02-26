777a1ff6441dd17ae37dd85772ff575fc1bb2015
mengxu <zhangmengxu@vanchen.com>
2026-02-26 16:32:30 +0800

    feat(controller): 添加日志记录功能到后端和营销

--- apps/backend/src/main/java/com/hxl/controller/BackendController.java
+++ apps/backend/src/main/java/com/hxl/controller/BackendController.java
@@ -1,11 +1,13 @@
 package com.hxl.controller;
 
 import com.hxl.feign.MarketingClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/backend")
 public class BackendController {
@@ -17,6 +19,8 @@ public class BackendController {
     public String doAction() {
         // 就像调用本地方法一样，发起微服务间远程 RPC 调用！
         // LoadBalancer 会自动去 Nacos 查 marketing-activity 的 IP，并执行轮询负载均衡
+
+        log.info("开始执行业务逻辑do-action...");
         String result = marketingClient.issueCoupon("U_8888");
         return "Backend 执行完毕。下游响应: " + result;
     }
--- apps/marketing-activity/src/main/java/com/hxl/controller/ActivityController.java
+++ apps/marketing-activity/src/main/java/com/hxl/controller/ActivityController.java
@@ -1,17 +1,20 @@
 package com.hxl.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 在 marketing-activity 服务的 Controller 中
+@Slf4j
 @RestController
 @RequestMapping("/api/marketing")
 public class ActivityController {
 
     @PostMapping("/issue-coupon")
     public String issueCoupon(@RequestParam("userId") String userId) {
+        log.info("模拟处理，用户 {} 请求发放优惠券", userId);
         // 模拟业务处理
         return "成功为用户 " + userId + " 发放了一张 100 元满减券！";
     }
